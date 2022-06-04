aa0faabc511e0fa96e6e849f42ca388e72a72e0e
ishant <ishantchauhan710@gmail.com>
2022-06-04 18:25:14 +0530

    Project Style Configuration Changes Made

--- app/src/main/java/com/docubox/DocuboxActivity.kt
+++ app/src/main/java/com/docubox/DocuboxActivity.kt
@@ -3,9 +3,9 @@ package com.docubox
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DocuboxActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_docubox)
     }
 }
