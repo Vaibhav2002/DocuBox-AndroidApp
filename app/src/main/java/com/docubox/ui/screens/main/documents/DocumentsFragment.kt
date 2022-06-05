a5b6948966a8443805c09bec669db191c8dc8fd0
ishant <ishantchauhan710@gmail.com>
2022-06-05 17:16:38 +0530

    Action Bar Designed

--- app/src/main/java/com/docubox/ui/screens/main/documents/DocumentsFragment.kt
+++ app/src/main/java/com/docubox/ui/screens/main/documents/DocumentsFragment.kt
@@ -1,6 +1,20 @@
 package com.docubox.ui.screens.main.documents
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.docubox.R
+import com.docubox.databinding.FragmentDocumentsBinding
+import com.docubox.util.viewBinding.viewBinding
 
-class DocumentsFragment : Fragment(R.layout.fragment_documents)
+class DocumentsFragment : Fragment(R.layout.fragment_documents) {
+
+    private val binding by viewBinding(FragmentDocumentsBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
