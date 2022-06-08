be99a3c54491d406bae591c35138610c899ad0e7
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-06-08 18:29:51 +0530

    Login Screen added

---
+++ app/src/main/java/com/docubox/ui/screens/auth/login/LoginFragment.kt
@@ -0,0 +1,60 @@
+package com.docubox.ui.screens.auth.login
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.docubox.R
+import com.docubox.databinding.FragmentLoginBinding
+import com.docubox.ui.screens.DocuBoxActivity
+import com.docubox.util.extensions.*
+import com.docubox.util.viewBinding.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    private val binding by viewBinding(FragmentLoginBinding::bind)
+    private val viewModel by viewModels<LoginViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        initListeners()
+        collectUIState()
+        collectUiEvents()
+    }
+
+    private fun collectUiEvents() = viewModel.events.launchAndCollect(viewLifecycleOwner) {
+        when (it) {
+            LoginScreenEvents.NavigateToMainScreen -> requireActivity().navigate(
+                DocuBoxActivity::class.java,
+                true
+            )
+            LoginScreenEvents.NavigateToRegisterScreen -> findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+            is LoginScreenEvents.ShowToast -> requireContext().showToast(it.message)
+        }
+    }
+
+    private fun collectUIState() = viewModel.uiState.launchAndCollectLatest(viewLifecycleOwner) {
+        // change progress bar visibiltiy acc to isLoading
+        with(binding) {
+            emailTIL.error = it.emailError
+            passwordTIL.error = it.passwordError
+            emailTIET.isEnabled = it.areTextFieldsEnabled
+            passwordTIET.isEnabled = it.areTextFieldsEnabled
+            loginBtn.isEnabled = it.isLoginButtonEnabled
+        }
+    }
+
+    private fun initListeners() = with(binding) {
+        emailTIET.doAfterTextChanged { viewModel.onEmailTextChange(it.toString()) }
+        passwordTIET.doAfterTextChanged { viewModel.onPasswordTextChange(it.toString()) }
+        loginBtn.singleClick(viewModel::onLoginButtonPressed)
+        goToRegister.singleClick(viewModel::onGoToRegisterPress)
+    }
+
+    private fun initViews() = with(binding) {}
+}
---
+++ app/src/main/java/com/docubox/util/CredentialsValidator.kt
@@ -0,0 +1,28 @@
+package com.docubox.util
+
+import android.text.TextUtils
+import android.util.Patterns
+
+private const val PASSWORD_LENGTH = 6
+
+fun String.validateEmail(): String? = when {
+    isEmpty() -> "Email cannot be empty"
+    !isValidEmail() -> "Invalid email"
+    else -> null
+}
+
+fun String.validatePassword(): String? = when {
+    isEmpty() -> "Password cannot be empty"
+    !isPasswordValid() -> "Minimum password length is $PASSWORD_LENGTH"
+    else -> null
+}
+
+fun String.validateConfirmPassword(password: String) = when {
+    password != this -> "Passwords do not match"
+    else -> null
+}
+
+fun String.isValidEmail() =
+    !TextUtils.isEmpty(this) && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+
+fun String.isPasswordValid() = isNotEmpty() && length >= PASSWORD_LENGTH
