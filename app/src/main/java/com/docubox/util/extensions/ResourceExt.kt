7ddc6b9c009f073e70b74f758873d86d16e454bf
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-06-08 18:29:29 +0530

    Resource classes and few extension functions added

---
+++ app/src/main/java/com/docubox/util/extensions/ResourceExt.kt
@@ -0,0 +1,20 @@
+package com.docubox.util.extensions
+
+import com.docubox.util.Resource
+
+inline infix fun <T, F> Resource<T>.mapTo(change: (T) -> F): Resource<F> = when (this) {
+    is Resource.Error -> Resource.Error(errorType, message)
+    is Resource.Loading -> Resource.Loading()
+    is Resource.Success -> Resource.Success(data?.let { change(it) }, message)
+}
+
+fun Resource<*>.mapToUnit() = this mapTo {}
+
+fun <T> Resource<T>.mapMessages(
+    successMessage: String? = null,
+    errorMessage: String? = null
+): Resource<T> = when (this) {
+    is Resource.Error -> copy(message = errorMessage ?: message)
+    is Resource.Success -> copy(message = successMessage ?: message)
+    else -> this
+}
