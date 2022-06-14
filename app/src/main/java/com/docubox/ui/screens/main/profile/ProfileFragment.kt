3d90fa554435fb37d4b8bfabc69a8bc6aa10aded
ishant <ishantchauhan710@gmail.com>
2022-06-14 23:23:20 +0530

    Profile Fragment UI Completed

--- app/src/main/java/com/docubox/ui/screens/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/docubox/ui/screens/main/profile/ProfileFragment.kt
@@ -5,11 +5,13 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.docubox.R
 import com.docubox.data.modes.local.User
 import com.docubox.databinding.FragmentProfileBinding
 import com.docubox.ui.screens.auth.AuthActivity
 import com.docubox.util.extensions.navigate
+import com.docubox.util.extensions.setupActionBar
 import com.docubox.util.extensions.showAlertDialog
 import com.docubox.util.viewBinding.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,8 +25,15 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initActionBar()
         initViews()
         initClickListeners()
+    }
+
+    private fun initActionBar() = with(binding) {
+        actionBar.setupActionBar("Profile",true,{
+            findNavController().popBackStack()
+        })
     }
 
     private fun initViews() = with(binding) {
