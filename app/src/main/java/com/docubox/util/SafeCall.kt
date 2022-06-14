a303c2b04f441129f84e1f62775233fd6bd792fc
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-06-14 18:17:23 +0530

    typo fixed

--- app/src/main/java/com/docubox/util/SafeCall.kt
+++ app/src/main/java/com/docubox/util/SafeCall.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.withContext
 import timber.log.Timber
 
 // Function to safely call a function and handle errors using timber logging and resource class
-fun <T> runsafe(call: () -> T): Resource<T> = try {
+fun <T> runSafe(call: () -> T): Resource<T> = try {
     val result = call()
     Resource.Success(result)
 } catch (e: Exception) {
@@ -13,7 +13,7 @@ fun <T> runsafe(call: () -> T): Resource<T> = try {
     Resource.Error(message = e.message.toString())
 }
 
-// Function to call runsafe function asynchronously
+// Function to call runSafe function asynchronously
 suspend fun <T> runSafeAsync(call: () -> T): Resource<T> = withContext(Dispatchers.IO) {
-    return@withContext runsafe(call)
+    return@withContext runSafe(call)
 }
