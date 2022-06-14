aa9600bb9fb2abdc2d51cef92c678a15d6dfd6a1
ishant <ishantchauhan710@gmail.com>
2022-06-14 23:54:55 +0530

    Home Fragment Custom Search Edit Text Designed

--- app/src/main/java/com/docubox/ui/screens/main/home/HomeFragment.kt
+++ app/src/main/java/com/docubox/ui/screens/main/home/HomeFragment.kt
@@ -1,6 +1,27 @@
 package com.docubox.ui.screens.main.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.docubox.R
+import com.docubox.databinding.FragmentHomeBinding
+import com.docubox.util.extensions.setupActionBar
+import com.docubox.util.viewBinding.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class HomeFragment : Fragment(R.layout.fragment_home)
+@AndroidEntryPoint
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private val binding by viewBinding(FragmentHomeBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initActionBar()
+    }
+
+    private fun initActionBar() = with(binding) {
+        actionBar.setupActionBar("Home")
+    }
+
+}
