3e9cb6161d2a0fb48fdcf2fcee42ea2d673f4587
ishantchauhan710 <ishantchauhan710@gmail.com>
2022-06-15 20:29:30 +0530

    File Picker Implemented

--- app/src/main/java/com/docubox/ui/screens/main/documents/DocumentsFragment.kt
+++ app/src/main/java/com/docubox/ui/screens/main/documents/DocumentsFragment.kt
@@ -1,7 +1,12 @@
 package com.docubox.ui.screens.main.documents
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -10,6 +15,7 @@ import com.docubox.R
 import com.docubox.data.modes.local.StorageItem
 import com.docubox.databinding.FragmentDocumentsBinding
 import com.docubox.databinding.ItemStorageBinding
+import com.docubox.databinding.SheetUploadDocumentBinding
 import com.docubox.ui.adapter.OneAdapter
 import com.docubox.util.extensions.*
 import com.docubox.util.viewBinding.viewBinding
@@ -67,18 +73,43 @@ class DocumentsFragment : Fragment(R.layout.fragment_documents) {
     }
 
     private fun openBottomSheet() {
+
+        val bottomSheetBinding = SheetUploadDocumentBinding.inflate(LayoutInflater.from(requireContext()))
+
         val bottomSheetDialog = BottomSheetDialog(
             requireContext(),
             R.style.DocuBox_BottomSheet
         ).apply {
-            setContentView(R.layout.sheet_upload_document)
-        }.also { it.show() }
+            setContentView(bottomSheetBinding.root)
+        }.also {
+            it.show()
+        }
+
+        bottomSheetBinding.btnUploadFile.setOnClickListener {
+            bottomSheetDialog.dismiss()
+            openFilePicker()
+        }
+
+    }
+
+    private val filePickerResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+
+            Toast.makeText(requireContext(),"Data recieved",Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun openFilePicker() = lifecycleScope.launchWhenStarted {
         askStoragePermission().also {
-            if (it) // open file picker
-                Timber.d("All permissions given")
+            if (it) {
+                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                    addCategory(Intent.CATEGORY_OPENABLE)
+                    type = "*/*"
+                }
+                filePickerResultLauncher.launch(intent)
+            }
+
         }
     }
 }
