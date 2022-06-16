1c559ebd6b09345f9d9f90a058ff171fcf484def
ishantchauhan710 <ishantchauhan710@gmail.com>
2022-06-16 23:20:26 +0530

    Profile Screen Fragment UI Redesigned

--- app/src/main/java/com/docubox/ui/screens/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/docubox/ui/screens/main/profile/ProfileFragment.kt
@@ -23,37 +23,39 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
     private val viewModel by viewModels<ProfileViewModel>()
     private var user: User ?= null
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initActionBar()
         initViews()
         initClickListeners()
     }
 
-    private fun initActionBar() = with(binding) {
-        actionBar.setupActionBar("Profile",true,{
-            findNavController().popBackStack()
-        })
-    }
 
-    private fun initViews() = with(binding) {
-        user = viewModel.getUser()
-        user?.let {
-            tvUserName.text = it.userName
-            tvUserEmail.text = it.userEmail
-        }
-    }
+   private fun initActionBar() = with(binding) {
+       actionBar.setupActionBar("Profile",true,{
+           findNavController().popBackStack()
+       })
+   }
 
-    private fun initClickListeners() = with(binding) {
-        logoutBtn.setOnClickListener {
-            lifecycleScope.launchWhenStarted {
-                val dialogAction = requireContext().showAlertDialog("Logout","Are you sure you want to logout?","Logout","Cancel")
-                if(dialogAction) {
-                    viewModel.logoutUser()
-                    requireActivity().navigate(AuthActivity::class.java,true)
-                }
-            }
-        }
-    }
+
+   private fun initViews() = with(binding) {
+       user = viewModel.getUser()
+       user?.let {
+           tvUserName.text = it.userName
+           tvUserEmail.text = it.userEmail
+       }
+   }
+
+   private fun initClickListeners() = with(binding) {
+       btnLogout.setOnClickListener {
+           lifecycleScope.launchWhenStarted {
+               val dialogAction = requireContext().showAlertDialog("Logout","Are you sure you want to logout?","Logout","Cancel")
+               if(dialogAction) {
+                   viewModel.logoutUser()
+                   requireActivity().navigate(AuthActivity::class.java,true)
+               }
+           }
+       }
+   }
 
 }
--- app/src/main/java/com/docubox/ui/screens/main/shared/SharedFragment.kt
+++ app/src/main/java/com/docubox/ui/screens/main/shared/SharedFragment.kt
@@ -1,6 +1,30 @@
 package com.docubox.ui.screens.main.shared
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.docubox.R
+import com.docubox.databinding.FragmentSharedBinding
+import com.docubox.util.extensions.setupActionBar
+import com.docubox.util.viewBinding.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class SharedFragment : Fragment(R.layout.fragment_shared)
+@AndroidEntryPoint
+class SharedFragment : Fragment(R.layout.fragment_shared) {
+
+    private val binding by viewBinding(FragmentSharedBinding::bind)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initActionBar()
+
+    }
+
+    private fun initActionBar() = with(binding) {
+        actionBar.setupActionBar("Shared", true, {
+            findNavController().popBackStack()
+        })
+    }
+
+
+}
