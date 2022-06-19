e55cc010ebc0f65da222e5a16e8a19a073ccc813
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-06-19 17:22:12 +0530

    File And Folder Options Menu added on Long Press

---
+++ app/src/main/java/com/docubox/ui/screens/dialogs/FileOptionsBottomSheetFragment.kt
@@ -0,0 +1,44 @@
+package com.docubox.ui.screens.dialogs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.docubox.data.modes.local.FileOption
+import com.docubox.databinding.ItemOptionsBinding
+import com.docubox.databinding.OptionsBottomSheetBinding
+import com.docubox.util.extensions.compose
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FileOptionsBottomSheetFragment(
+    private val items: List<FileOption> = emptyList(),
+    private val onOptionSelected: (FileOption) -> Unit = {}
+) : BottomSheetDialogFragment() {
+
+    private lateinit var binding: OptionsBottomSheetBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = OptionsBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.optionsRv.setHasFixedSize(false)
+        binding.optionsRv.compose(
+            ItemOptionsBinding::inflate,
+            onBind = { item: FileOption, pos ->
+                text.text = item.text
+            },
+        ) {
+            onOptionSelected(this)
+            dismiss()
+        }.apply { submitList(items) }
+    }
+}
---
+++ app/src/main/java/com/docubox/ui/screens/dialogs/FolderOptionsBottomSheetFragment.kt
@@ -0,0 +1,44 @@
+package com.docubox.ui.screens.dialogs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.docubox.data.modes.local.FolderOptions
+import com.docubox.databinding.ItemOptionsBinding
+import com.docubox.databinding.OptionsBottomSheetBinding
+import com.docubox.util.extensions.compose
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FolderOptionsBottomSheetFragment(
+    private val items: List<FolderOptions> = emptyList(),
+    private val onOptionSelected: (FolderOptions) -> Unit = {}
+) : BottomSheetDialogFragment() {
+
+    private lateinit var binding: OptionsBottomSheetBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = OptionsBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.optionsRv.setHasFixedSize(false)
+        binding.optionsRv.compose(
+            ItemOptionsBinding::inflate,
+            onBind = { item: FolderOptions, pos ->
+                text.text = item.text
+            },
+        ) {
+            onOptionSelected(this)
+            dismiss()
+        }.apply { submitList(items) }
+    }
+}
