d4645e8c3d5f3ebeea8c244ae9d518577c34813f
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-06-19 14:12:41 +0530

    Text Input Material Dialog Added

--- app/src/main/java/com/docubox/util/extensions/ContextExtensions.kt
+++ app/src/main/java/com/docubox/util/extensions/ContextExtensions.kt
@@ -1,7 +1,9 @@
 package com.docubox.util.extensions
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.widget.Toast
+import com.docubox.databinding.TextInputDialogBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
@@ -30,4 +32,26 @@ suspend fun Context.showAlertDialog(
 // Function to show a toast message
 fun Context.showToast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+
+suspend fun Context.showInputDialog(
+    title: String,
+    text: String
+) = suspendCoroutine<String> {
+    val binding = TextInputDialogBinding.inflate(LayoutInflater.from(this)).apply {
+        nameTIET.setText(text)
+    }
+
+    MaterialAlertDialogBuilder(this).apply {
+        setTitle(title)
+        setView(binding.root)
+        setPositiveButton("Confirm") { _, _ ->
+            it.resume(binding.nameTIET.text.toString())
+        }
+        setNegativeButton("Cancel") { _, _ ->
+            it.resume(text)
+        }
+        show()
+    }
 }
