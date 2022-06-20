d937f3954628cb533e3d9ab482e1475763abbff7
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-06-20 12:56:25 +0530

    Share File API integrated in Data Layer

--- app/src/main/java/com/docubox/data/modes/remote/MessageResponse.kt
+++ app/src/main/java/com/docubox/data/modes/remote/MessageResponse.kt
@@ -2,7 +2,7 @@ package com.docubox.data.modes.remote
 
 import com.google.gson.annotations.SerializedName
 
-data class ErrorDto(
+data class MessageResponse(
     @SerializedName("message")
     val message: String = ""
 )
--- app/src/main/java/com/docubox/data/remote/dataSources/StorageDataSource.kt
+++ app/src/main/java/com/docubox/data/remote/dataSources/StorageDataSource.kt
@@ -3,6 +3,7 @@ package com.docubox.data.remote.dataSources
 import com.docubox.data.modes.remote.requests.CreateFolderRequest
 import com.docubox.data.modes.remote.requests.GetFileRequest
 import com.docubox.data.modes.remote.requests.GetFolderRequest
+import com.docubox.data.modes.remote.requests.ShareFileRequest
 import com.docubox.data.remote.api.StorageService
 import com.docubox.util.extensions.asJwt
 import com.docubox.util.safeApiCall
@@ -29,7 +30,12 @@ class StorageDataSource @Inject constructor(private val service: StorageService)
     suspend fun getFilesSharedByMe(token: String) = safeApiCall {
         service.getFilesSharedByMe(token.asJwt())
     }
+
     suspend fun getFilesSharedToMe(token: String) = safeApiCall {
         service.getFilesSharedToMe(token.asJwt())
+    }
+
+    suspend fun shareFile(fileId: String, toEmail: String, token: String) = safeApiCall {
+        service.shareFile(ShareFileRequest(fileId, toEmail), token.asJwt())
     }
 }
