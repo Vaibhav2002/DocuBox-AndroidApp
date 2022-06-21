6cf0f609dda3591965536425cdde78d4a7cd52ff
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-06-21 14:49:30 +0530

    Remote to Local data mapping added in repo method pipeline

--- app/src/main/java/com/docubox/data/repo/StorageRepo.kt
+++ app/src/main/java/com/docubox/data/repo/StorageRepo.kt
@@ -95,5 +95,7 @@ class StorageRepo @Inject constructor(
     suspend fun searchFileByQuery(query: String) = flow {
         emit(Resource.Loading())
         emit(storageDataSource.searchFilesByName(query, token))
+    }.map { res ->
+        res.mapTo { fileMapper.toLocal(it.fileList) }
     }.flowOn(Dispatchers.IO)
 }
