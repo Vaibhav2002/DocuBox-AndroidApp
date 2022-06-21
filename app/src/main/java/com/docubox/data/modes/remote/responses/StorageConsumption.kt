b34d26144806c85e77adc58391d00f387a76b152
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-06-21 14:10:51 +0530

    Storage Consumption API integrated in UI Layer

--- app/src/main/java/com/docubox/data/modes/remote/responses/StorageConsumption.kt
+++ app/src/main/java/com/docubox/data/modes/remote/responses/StorageConsumption.kt
@@ -5,5 +5,7 @@ import com.google.gson.annotations.SerializedName
 
 data class StorageConsumption(
     @SerializedName("storageConsumption")
-    val storageConsumption: String = ""
+    val storageConsumption: String = "",
+    @SerializedName("totalStorage")
+    val totalStorage: String = "50"
 )
