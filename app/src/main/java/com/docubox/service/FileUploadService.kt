3d2c9a579d9d22276d4af5d2125ec610233f5c11
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-06-23 14:43:45 +0530

    File Size Validation at upload refactored

--- app/src/main/java/com/docubox/service/FileUploadService.kt
+++ app/src/main/java/com/docubox/service/FileUploadService.kt
@@ -7,7 +7,9 @@ import android.os.Binder
 import android.os.IBinder
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleService
+import com.docubox.util.FileUtil
 import com.docubox.util.Secrets.BASE_URL
+import com.docubox.util.extensions.showToast
 import net.gotev.uploadservice.data.UploadInfo
 import net.gotev.uploadservice.network.ServerResponse
 import net.gotev.uploadservice.observer.request.RequestObserverDelegate
@@ -19,7 +21,7 @@ import kotlin.coroutines.suspendCoroutine
 class FileUploadService : LifecycleService() {
 
     companion object {
-        private val UPLOAD_FILE_URL = BASE_URL + "documents/create-file"
+        private const val UPLOAD_FILE_URL = BASE_URL + "documents/create-file"
     }
 
     private val binder = FileUploadBinder()
@@ -36,43 +38,48 @@ class FileUploadService : LifecycleService() {
     suspend fun uploadFile(
         file: Uri,
         fileDirectory: String?,
+        storageLeft:Float,
         token: String,
         lifeCycleOwner: LifecycleOwner
-    ) =
-        suspendCoroutine<Boolean> {
-            val directory = fileDirectory?.let { dir -> listOf(dir) } ?: emptyList()
-            MultipartUploadRequest(this@FileUploadService, UPLOAD_FILE_URL)
-                .setMethod("POST")
-                .addFileToUpload(filePath = file.toString(), parameterName = "upload")
-                .addArrayParameter("fileDirectory", directory)
-                .setBearerAuth(token)
-                .subscribe(
-                    this@FileUploadService,
-                    lifeCycleOwner,
-                    object : RequestObserverDelegate {
-                        override fun onCompleted(context: Context, uploadInfo: UploadInfo) = Unit
-
-                        override fun onCompletedWhileNotObserving() = Unit
-
-                        override fun onError(
-                            context: Context,
-                            uploadInfo: UploadInfo,
-                            exception: Throwable
-                        ) {
-                            Timber.d("DOCUBOX_ERROR: ${exception.message}")
-                            it.resume(false)
-                        }
-
-                        override fun onProgress(context: Context, uploadInfo: UploadInfo) = Unit
-
-                        override fun onSuccess(
-                            context: Context,
-                            uploadInfo: UploadInfo,
-                            serverResponse: ServerResponse
-                        ) {
-                            it.resume(true)
-                        }
-                    })
+    ) = suspendCoroutine<Boolean> {
+        val fileSize = FileUtil.getFileSize(this, file)
+        if (fileSize > storageLeft) {
+            showToast("Unable to upload file, you have crossed your file storage limit")
+            it.resume(false)
         }
+        val directory = fileDirectory?.let { dir -> listOf(dir) } ?: emptyList()
+        MultipartUploadRequest(this@FileUploadService, UPLOAD_FILE_URL)
+            .setMethod("POST")
+            .addFileToUpload(filePath = file.toString(), parameterName = "upload")
+            .addArrayParameter("fileDirectory", directory)
+            .setBearerAuth(token)
+            .subscribe(
+                this@FileUploadService,
+                lifeCycleOwner,
+                object : RequestObserverDelegate {
+                    override fun onCompleted(context: Context, uploadInfo: UploadInfo) = Unit
+
+                    override fun onCompletedWhileNotObserving() = Unit
+
+                    override fun onError(
+                        context: Context,
+                        uploadInfo: UploadInfo,
+                        exception: Throwable
+                    ) {
+                        Timber.d("DOCUBOX_ERROR: ${exception.message}")
+                        it.resume(false)
+                    }
+
+                    override fun onProgress(context: Context, uploadInfo: UploadInfo) = Unit
+
+                    override fun onSuccess(
+                        context: Context,
+                        uploadInfo: UploadInfo,
+                        serverResponse: ServerResponse
+                    ) {
+                        it.resume(true)
+                    }
+                })
+    }
 
 }
