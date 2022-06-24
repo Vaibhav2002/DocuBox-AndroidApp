14903cfe74e3680ba91911a9df63cc717148e399
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-06-24 20:08:54 +0530

    Few improvements and bug fixes

--- app/src/main/java/com/docubox/data/modes/local/FileType.kt
+++ app/src/main/java/com/docubox/data/modes/local/FileType.kt
@@ -8,11 +8,12 @@ import com.google.gson.annotations.SerializedName
 sealed class FileType(
     val type: String,
     @DrawableRes val icon: Int,
-    val mimeType:String
+    val mimeType:String,
+    val title:String = mimeType
 ) {
-    object Audio : FileType("Audio", R.drawable.ic_audio, "audio")
-    object Document : FileType("Documents", R.drawable.ic_document, "application/pdf")
-    object Image : FileType("Image", R.drawable.ic_image, "image")
-    object Video : FileType("Video", R.drawable.ic_video, "video")
-    object File : FileType("File", R.drawable.ic_document, "application")
+    object Audio : FileType("Audio", R.drawable.ic_audio, "audio", "Audio")
+    object Document : FileType("Documents", R.drawable.ic_document, "application/pdf", "Documents")
+    object Image : FileType("Image", R.drawable.ic_image, "image", "Images")
+    object Video : FileType("Video", R.drawable.ic_video, "video", "Videos")
+    object File : FileType("File", R.drawable.ic_document, "application", "Documents")
 }
