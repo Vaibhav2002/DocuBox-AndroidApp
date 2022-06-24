d8bd84e9d0ba1bdf972867ef846afa6731506a88
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2022-06-24 14:53:24 +0530

    Profile Screen click handlers added

--- app/src/main/java/com/docubox/ui/screens/main/profile/ProfileViewModel.kt
+++ app/src/main/java/com/docubox/ui/screens/main/profile/ProfileViewModel.kt
@@ -1,22 +1,23 @@
 package com.docubox.ui.screens.main.profile
 
 import androidx.lifecycle.ViewModel
-import com.docubox.data.modes.local.User
+import androidx.lifecycle.viewModelScope
+import com.docubox.data.repo.AuthRepo
 import com.docubox.data.repo.PreferencesRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-    private val preferencesRepo: PreferencesRepo, // Repository that contains data store functions
-): ViewModel() {
+    private val preferencesRepo: PreferencesRepo,
+    private val authRepo: AuthRepo, // Repository that contains data store functions
+) : ViewModel() {
     // Function to get a user from datastore using Preference Repository
-    fun getUser(): User? {
-        return preferencesRepo.getUser()
-    }
+    fun getUser() = preferencesRepo.getUser()
 
-    fun logoutUser() {
-        return preferencesRepo.removeUser()
+    fun logoutUser() = viewModelScope.launch {
+        authRepo.logoutUser()
     }
 
 }
